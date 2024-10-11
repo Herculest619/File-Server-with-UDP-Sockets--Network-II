c9134f91838348e34dd26ab184914d0e303fb6ad
Hércules Teixeira <herculesteixeira619@gmail.com>
2024-10-11 19:43:37 -0300

    hash MD5 servidor para cliente

--- Client/Cliente.py
+++ Client/Cliente.py
@@ -1,33 +1,50 @@
-import socket, os, sys, random
+import socket, os, sys, random, hashlib
 
-HOST = '127.0.0.1'  # endereço IP
-PORT = 2002         # Porta utilizada pelo servidor
-BUFFER_SIZE = 1460  # tamanho do buffer para recepção dos dados
-TIMEOUT = 0.2       # Timeout de 1 segundo
+HOST = '127.0.0.1'
+PORT = 2002
+BUFFER_SIZE = 1460
+TIMEOUT = 0.2
+
+# Função para calcular o hash MD5 de um arquivo
+def calcular_hash_md5(caminho_arquivo):
+    md5_hash = hashlib.md5()
+    with open(caminho_arquivo, 'rb') as f:
+        while chunk := f.read(BUFFER_SIZE):
+            md5_hash.update(chunk)
+    return md5_hash.hexdigest()
 
 def receber_arquivo(UDPClientSocket, file_name):
     current_directory = os.path.dirname(os.path.abspath(__file__))
     file_path = os.path.join(current_directory, file_name)
 
     with open(file_path, 'wb') as arquivo:
-                while True:
-                    
-                    msgFromServer = UDPClientSocket.recvfrom(BUFFER_SIZE)
-                    # Simula uma perda de pacote de 5%
-                    if random.random() < 0.95:
-                        data = msgFromServer[0]
-                        arquivo.write(data)
+        while True:
+            msgFromServer = UDPClientSocket.recvfrom(BUFFER_SIZE)
+            if random.random() < 0.95:
+                data = msgFromServer[0]
+                arquivo.write(data)
+                UDPClientSocket.sendto("ACK".encode(), (HOST, PORT))
 
-                        # Envia um ACK para o servidor
-                        UDPClientSocket.sendto("ACK".encode(), (HOST, PORT))
+                if len(data) < BUFFER_SIZE:
+                    break
+            else:
+                print("Falha simula de perda de pacote!")
 
-                        if len(data) < BUFFER_SIZE:
-                            break
-                    else:
-                        print("Falha simula de perda de pacote!")
+        print(f"\nArquivo '{file_name}' baixado com sucesso!\n")
 
-                print(f"\nArquivo '{file_name}' baixado com sucesso!\n")
+    # Receber e verificar o hash MD5
+    hash_md5_servidor, _ = UDPClientSocket.recvfrom(BUFFER_SIZE)
+    hash_md5_servidor = hash_md5_servidor.decode()
+    print(f"Hash MD5 recebido do servidor: {hash_md5_servidor}")
 
+    # Calcular o hash MD5 do arquivo recebido
+    hash_md5_local = calcular_hash_md5(file_path)
+    print(f"Hash MD5 do arquivo baixado: {hash_md5_local}")
+
+    if hash_md5_servidor == hash_md5_local:
+        print("Verificação concluída com sucesso: os hashes coincidem.")
+    else:
+        print("Falha na verificação: os hashes não coincidem.")
 
 def envia_arquivo(UDPClientSocket, file_name):
     try:
@@ -47,7 +64,6 @@ def envia_arquivo(UDPClientSocket, file_name):
                         print(f"Simulação de falha de envio do pacote {i}")
                         continue
 
-                    # Esperar confirmação do servidor
                     try:
                         UDPClientSocket.settimeout(TIMEOUT)
                         ack, _ = UDPClientSocket.recvfrom(BUFFER_SIZE)
@@ -60,53 +76,43 @@ def envia_arquivo(UDPClientSocket, file_name):
     except Exception as e:
         print(f"Erro ao enviar o arquivo: {e}")
 
-
 def main(argv):
     try:
-        # Cria o socket UDP
         with socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM) as UDPClientSocket:
-            
-            opcao = input("\nDeseja baixar ou enviar arquivo?\n 1:DONLOAD \n 2:UPLOAD \n").lower()
-            
+            opcao = input("\nDeseja baixar ou enviar arquivo?\n 1: DOWNLOAD \n 2: UPLOAD \n").lower()
+
             if opcao == '1':
-                UDPClientSocket.sendto("DOWNLOAD".encode(), (HOST, PORT))  # Solicita a lista de arquivos
-                # (código para download permanece igual)
-                msgFromServer = UDPClientSocket.recvfrom(BUFFER_SIZE)  # Recebe a mensagem do servidor
-                msg = msgFromServer[0].decode()  # Decodifica a mensagem recebida para uma string
+                UDPClientSocket.sendto("DOWNLOAD".encode(), (HOST, PORT))
+                msgFromServer = UDPClientSocket.recvfrom(BUFFER_SIZE)
+                msg = msgFromServer[0].decode()
+                arquivos = eval(msg)
 
-                arquivos = eval(msg)  # Converte a string de volta para um dicionário
-
-                # Formata a mensagem de forma legível
                 print("\nLista de arquivos do servidor:\n")
                 for indice, nome_arquivo in arquivos.items():
-                    print(f"{indice}: {nome_arquivo}")  # Exibe o índice e o nome do arquivo, um por linha
+                    print(f"{indice}: {nome_arquivo}")
 
-                # Recebe o índice do arquivo a ser baixado
                 opcao = input("\nDigite o índice do arquivo a ser baixado: ")
                 UDPClientSocket.sendto(opcao.encode(), (HOST, PORT))
 
-                # Recebe o arquivo, ele virá em partes devido ao tamanho do buffer de 1460 bytes
                 opcao = int(opcao)
                 file_name = arquivos[opcao]
-
                 receber_arquivo(UDPClientSocket, file_name)
-                
+
             elif opcao == '2':
-                caminho_atual = os.path.dirname(os.path.abspath(__file__))  # Caminho absoluto do arquivo atual
-                arquivos = os.listdir(caminho_atual)  # Lista de arquivos da pasta atual, usando dicionário
-                arquivos = [arquivo for arquivo in arquivos if not arquivo.endswith('.py')]  # Filtrar arquivos para não listar arquivos .py
-                arquivos = {i: arquivo for i, arquivo in enumerate(arquivos)}  # Adicionar um número identificador para cada arquivo
+                caminho_atual = os.path.dirname(os.path.abspath(__file__))
+                arquivos = os.listdir(caminho_atual)
+                arquivos = [arquivo for arquivo in arquivos if not arquivo.endswith('.py')]
+                arquivos = {i: arquivo for i, arquivo in enumerate(arquivos)}
                 print("\nArquivos disponíveis: ")
                 print(arquivos)
 
                 opcao = input("\nDigite o índice do arquivo a ser enviado: ")
                 opcao = int(opcao)
-                file_name = arquivos[opcao] # Nome do arquivo a ser enviado
+                file_name = arquivos[opcao]
 
-                UDPClientSocket.sendto("UPLOAD".encode(), (HOST, PORT))  # Informa o servidor que quer fazer upload
-                UDPClientSocket.sendto(file_name.encode(), (HOST, PORT))  # Envia o nome do arquivo
-                envia_arquivo(UDPClientSocket, file_name)  # Envia o arquivo
-
+                UDPClientSocket.sendto("UPLOAD".encode(), (HOST, PORT))
+                UDPClientSocket.sendto(file_name.encode(), (HOST, PORT))
+                envia_arquivo(UDPClientSocket, file_name)
 
     except Exception as error:
         print("Exceção - Programa será encerrado!")
--- Server/Server.py
+++ Server/Server.py
@@ -1,13 +1,20 @@
-import socket, sys ,os, random
+import socket, sys, os, random, hashlib
 from threading import Thread
 
-HOST = '127.0.0.1'  # endereço IP
-PORT = 2002         # Porta utilizada pelo servidor
-BUFFER_SIZE = 1460  # tamanho do buffer para recepção dos dados
-TIMEOUT = 0.2       # Timeout de 1 segundo
+HOST = '127.0.0.1'
+PORT = 2002
+BUFFER_SIZE = 1460
+TIMEOUT = 0.2
+
+# Função para calcular o hash MD5 de um arquivo
+def calcular_hash_md5(caminho_arquivo):
+    md5_hash = hashlib.md5()
+    with open(caminho_arquivo, 'rb') as f:
+        while chunk := f.read(BUFFER_SIZE):
+            md5_hash.update(chunk)
+    return md5_hash.hexdigest()
 
 def envia_arquivo(arquivo, endereco, caminho_arquivo, UDPServerSocket):
-    # Enviar o arquivo para o cliente, fragmentando em 1460 bytes devido ao tamanho do buffer
     with open(caminho_arquivo, 'rb') as file:
         i = 0
         while True:
@@ -15,44 +22,41 @@ def envia_arquivo(arquivo, endereco, caminho_arquivo, UDPServerSocket):
             if not dados:
                 break
             while True:
-                # Simulate a 5% error rate
                 if random.random() < 0.95:
-                    # Enviar pacote
                     UDPServerSocket.sendto(dados, endereco)
                 else:
                     print(f"Falha simulada de envio de pacote {i}")
                     continue
 
-                # Esperar confirmação do cliente
                 try:
-                    UDPServerSocket.settimeout(TIMEOUT)  # Timeout de 1 segundo
+                    UDPServerSocket.settimeout(TIMEOUT)
                     ack, _ = UDPServerSocket.recvfrom(BUFFER_SIZE)
                     if ack.decode() == 'ACK':
-                        #print(f"pacote {i} enviado com sucesso")
                         i += 1
                         break
                     else:
                         print(f"Falha ao receber ACK do pacote {i}")
                 except socket.timeout:
                     print(f"Timeout no pacote {i}, reenviando...")
-    print("\nArquivo enviado com sucesso!!")
-    print("Foram enviados ", i, " pacotes\n\n")
 
+    print("\nArquivo enviado com sucesso!!")
+    print(f"Foram enviados {i} pacotes\n")
+
+    # Calcular e enviar o hash MD5 do arquivo
+    hash_md5 = calcular_hash_md5(caminho_arquivo)
+    UDPServerSocket.sendto(hash_md5.encode(), endereco)
+    print(f"Hash MD5 do arquivo enviado: {hash_md5}")
 
 def receber_arquivo(UDPServerSocket, endereco, nome_arquivo):
     try:
-        # Caminho para salvar o arquivo recebido
         caminho_atual = os.path.dirname(os.path.abspath(__file__))
         caminho_arquivo = os.path.join(caminho_atual, nome_arquivo)
 
         with open(caminho_arquivo, 'wb') as arquivo:
             while True:
-                # Recebe dados do cliente
                 dados, addr = UDPServerSocket.recvfrom(BUFFER_SIZE)
                 if random.random() < 0.95:
                     arquivo.write(dados)
-
-                    # Envia um ACK para o cliente
                     UDPServerSocket.sendto("ACK".encode(), endereco)
 
                     if len(dados) < BUFFER_SIZE:
@@ -63,64 +67,51 @@ def receber_arquivo(UDPServerSocket, endereco, nome_arquivo):
     except Exception as e:
         print(f"Erro ao receber o arquivo: {e}")
 
-
 def main(argv):
     try:
-        # Cria um socket UDP
         UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-        UDPServerSocket.bind((HOST, PORT))  # Escuta na porta especificada
+        UDPServerSocket.bind((HOST, PORT))
         print("\nUDP server up and listening")
 
-        # Espera por conexões
         while True:
-            bytesAddressPair = UDPServerSocket.recvfrom(BUFFER_SIZE)  # recebe a mensagem do cliente
-            endereco = bytesAddressPair[1]  # endereço do cliente
+            bytesAddressPair = UDPServerSocket.recvfrom(BUFFER_SIZE)
+            endereco = bytesAddressPair[1]
             
             if bytesAddressPair[0].decode() == 'UPLOAD':
-                # Receber nome do arquivo
                 nome_arquivo, _ = UDPServerSocket.recvfrom(BUFFER_SIZE)
                 nome_arquivo = nome_arquivo.decode()
                 print(f"Cliente deseja enviar o arquivo: {nome_arquivo}")
-
-                # Chamar a função para receber o arquivo
                 receber_arquivo(UDPServerSocket, endereco, nome_arquivo)
                 break
 
             elif bytesAddressPair[0].decode() == 'DOWNLOAD':
-                clientIP = "Client IP Address:{}".format(endereco)  # endereço do cliente
+                clientIP = f"Client IP Address: {endereco}"
                 print("\n" + clientIP)
 
-                caminho_atual = os.path.dirname(os.path.abspath(__file__))  # Caminho absoluto do arquivo atual
-                arquivos = os.listdir(caminho_atual)  # Lista de arquivos da pasta atual, usando dicionário
-                arquivos = [arquivo for arquivo in arquivos if not arquivo.endswith('.py')]  # Filtrar arquivos para não listar arquivos .py
-                arquivos = {i: arquivo for i, arquivo in enumerate(arquivos)}  # Adicionar um número identificador para cada arquivo
+                caminho_atual = os.path.dirname(os.path.abspath(__file__))
+                arquivos = os.listdir(caminho_atual)
+                arquivos = [arquivo for arquivo in arquivos if not arquivo.endswith('.py')]
+                arquivos = {i: arquivo for i, arquivo in enumerate(arquivos)}
                 print("\nArquivos disponíveis: ")
                 print(arquivos)
 
-                # Enviar a lista de arquivos para o cliente
                 UDPServerSocket.sendto(str(arquivos).encode(), endereco)
-
-                # Receber a opção do cliente para download
                 bytesAddressPair = UDPServerSocket.recvfrom(BUFFER_SIZE)
                 opcao = int(bytesAddressPair[0].decode())
-                print("\nOpção escolhida pelo cliente: ", opcao)
+                print(f"\nOpção escolhida pelo cliente: {opcao}")
 
-                # Enviar o arquivo escolhido pelo cliente
                 arquivo = arquivos[opcao]
-                print("\nArquivo escolhido pelo cliente: ", arquivo)
+                print(f"\nArquivo escolhido pelo cliente: {arquivo}")
                 caminho_arquivo = os.path.join(caminho_atual, arquivo)
-                print("\nCaminho do arquivo: ", caminho_arquivo)
+                print(f"\nCaminho do arquivo: {caminho_arquivo}")
 
                 envia_arquivo(arquivo, endereco, caminho_arquivo, UDPServerSocket)
                 break
-            
     except Exception as error:
         print("Erro na execução do servidor!!")
         print(error)
         return
 
-
 if __name__ == "__main__":
     while True:
         main(sys.argv[1:])
-
